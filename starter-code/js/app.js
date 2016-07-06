bc0a2e9c96707ba3b255aa8dd2af30e08f57a886
Kate Nelson <mkatenelson@gmail.com>
2016-07-06 12:06:03 -0700

    completed in-class

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,4 +1,19 @@
 console.log('app.js loaded!');
 
+// APP
 // initialize the application
-var app = angular.module("hangmanApp", []);
+angular
+  .module("hangmanApp", [])
+  .controller("hangmanCtrl", hangmanCtrl);
+
+// CONTROLLER
+hangmanCtrl.$inject = ["$scope"];
+  function hangmanCtrl($scope) {
+    // new game
+    $scope.hangman = new HangmanGame("hiphopopotomous");
+    $scope.guessLetter = function (input) {
+      $scope.hangman.guess(input);
+      $scope.hangman.input = "";
+    };
+
+  }
